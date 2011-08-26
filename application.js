76040d3a3b40891a0a29ba705684e3d8967823a0
Paul Campbell <paulgcampbell@gmail.com>
2011-08-26 11:39:45 +0100

    making the menu active

---
+++ application.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+  var activeTarget,
+      $window = $(window),
+      position = {},
+      nav = $('.topbar li a'),
+      targets = nav.map(function () {
+        return $(this).attr('href');
+      }),
+      offsets = $.map(targets, function (id) {
+        return $(id).offset().top -150;
+      });
+
+
+  function setButton(id) {
+    nav.parent("li").removeClass('active');
+    $(nav[$.inArray(id, targets)]).parent("li").addClass('active');
+  }
+
+  function processScroll(e) {
+    var scrollTop = $window.scrollTop() + 10, i;
+    for (i = offsets.length; i--;) {
+      if (activeTarget != targets[i] && scrollTop >= offsets[i] && (!offsets[i + 1] || scrollTop <= offsets[i + 1])) {
+        activeTarget = targets[i];
+        setButton(activeTarget);
+      }
+    }
+  }
+
+  nav.click(function () {
+    processScroll();
+  });
+
+  processScroll();
+
+  $window.scroll(processScroll);
+
+});
